4524bec46b68b42feed7dfda85fa2543f33931f7
philopon <philopon.dependence@gmail.com>
2014-08-25 00:35:40 +0900

    add ranking, package JSON API.

--- src/best-haskell.hs
+++ src/best-haskell.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 import Control.Exception
 import Control.Applicative
@@ -11,15 +14,19 @@ import Control.Monad.Trans.Control
 import System.Environment
 
 import qualified Data.Text as T
+import qualified Data.ByteString.Char8 as S8
 
 import Web.Apiary
 import Network.Wai(Application)
 import Network.Wai.Handler.Warp
 
+import Data.Word
 import Data.Pool
+import Data.Typeable
+import Data.Maybe
 import Data.Time
-import Data.Int
 import Data.Reflection
+import qualified Data.Aeson as JSON
 
 import qualified Database.MongoDB as Mongo
 
@@ -58,28 +65,103 @@ auth = do
     b <- Mongo.auth (mongoUser given) (mongoPassword given)
     unless b $ fail "MongoDB auth failed."
 
+newtype Since = Since {getDay :: Day} deriving (Typeable, Show)
+
+instance Query Since where
+    readQuery = (>>= \s0 -> do
+        (y, s1) <- S8.readInt s0
+        (m, s2) <- S8.readInt (S8.tail s1)
+        (d,  _) <- S8.readInt (S8.tail s2)
+        let y' = if y < 100 then 2000 + y else y
+        return . Since $ fromGregorian (fromIntegral y') m d)
+    qTypeRep _ = typeOf (undefined :: Day)
+
+-- member
+-- library
+-- executable
+-- license
+-- executables
+-- category
+
+rankingAction :: (MonadIO m, MonadBaseControl IO m, Given MongoState)
+              => Maybe Word -> Maybe Word -> Maybe Since -> ActionT m ()
+rankingAction mblimit mbrange mbsince = do
+    contentType "application/json"
+
+    let limit = maybe 10 (min 100 . fromIntegral) mblimit
+
+    since <- case mbsince of
+        Just s  -> return $ getDay s
+        Nothing ->
+            addDays (negate . fromIntegral $ maybe 7 (min 100) mbrange)
+            . utctDay <$> liftIO getCurrentTime
+
+    doc <- access $ do
+        auth
+        rank <- Mongo.aggregate "downloads"
+            [aggrSinceDate since, aggrGroupByPackage, aggrSort, aggrLimit limit]
+        catMaybes <$> mapM (\d -> fmap (Mongo.merge d) <$>
+            Mongo.findOne (Mongo.select ["_id" Mongo.=: Mongo.valueAt "_id" d] "cabal")) rank
+
+    lazyBytes . JSON.encode $ map (toAeson . Mongo.include cabalFields) doc
+
+
 application :: Mongo.Username -> Mongo.Password -> T.Text -> Pool Mongo.Pipe -> Application
 application muser mpasswd mdb pool = runApiary def $ give (MongoState pool muser mpasswd mdb) $ do
-    root . action $ do
-        doc <- access $ do
-            auth
-            let day = ModifiedJulianDay 56886
-            Mongo.aggregate "downloads" $ pipeline day 10
-        contentType "text/html"
-        mapM_ (\a -> showing (toAeson a) >> char '\n') doc
+    [capture|/ranking|] . method GET $ do
+        eqHeader "Accept" "application/json"
+            . ("limit" ?? "number to fetch ranking(default: 10)."  =?: pWord) 
+            . ("range" ?? "days of aggregation(default: 7)."       =?: pWord)
+            . ("since" ?? "date of aggregation. instead of range." =?: (Proxy :: Proxy Since))
+            . document "get package download ranking."
+            $ action rankingAction
 
-pipeline :: Day -> Int64 -> Mongo.Pipeline
-pipeline since num = [match, grouping, sorting, limit]
-  where
-    match =
-        [ "$match" Mongo.=: 
-            [ "date" Mongo.=: [ "$gt" Mongo.=: Mongo.UTC (UTCTime since 0)] ]
-        ]
-    grouping =
-        [ "$group" Mongo.=:
-            [ "_id"   Mongo.=: Mongo.String "$package"
-            , "total" Mongo.=: ["$sum" Mongo.=: Mongo.String "$count"]
-            ]
-        ]
-    sorting = ["$sort" Mongo.=: ["total" Mongo.=: Mongo.Int64 (-1)]]
-    limit   = ["$limit" Mongo.=: Mongo.Int64 num]
+    [capture|/package/:T.Text[package name]|] . method GET $ do
+        eqHeader "Accept" "application/json"
+            . document "get package information."
+            . action $ \pkg -> do
+                join . access $ do
+                    auth
+                    Mongo.findOne (Mongo.select ["package" Mongo.=: pkg] "cabal") >>= \case
+                        Nothing    -> return $
+                            status status404 >> bytes "unknown package" >> stop
+
+                        Just pinfo -> do
+                            ds <- Mongo.rest =<< Mongo.find 
+                                (Mongo.select ["package" Mongo.=: Mongo.valueAt "_id" pinfo] "downloads")
+                                {Mongo.project = [ "count" Mongo.=: Mongo.Int64 1
+                                                 , "date"  Mongo.=: Mongo.Int64 1
+                                                 ]}
+                            let ds' = map (Mongo.include ["date", "count"]) ds
+                            return $ do
+                                contentType "application/json"
+                                let doc = ("downloads" Mongo.=: ds') : pinfo
+                                lazyBytes . JSON.encode $ toAeson doc
+
+    [capture|/api/documentation|] . method GET . action $
+        defaultDocumentationAction def
+
+cabalFields :: [Mongo.Label]
+cabalFields = [ "homepage", "copyright", "category"
+              , "members", "maintainer", "version"
+              , "synopsis", "package", "has_library"
+              , "total", "stability", "author"
+              , "bugReports", "license", "executables"
+              , "description"
+              ]
+
+aggrSinceDate :: Day -> Mongo.Document
+aggrSinceDate since = [ "$match" Mongo.=: 
+    [ "date" Mongo.=: [ "$gte" Mongo.=: Mongo.UTC (UTCTime since 0)] ] ]
+
+aggrGroupByPackage :: Mongo.Document
+aggrGroupByPackage = [ "$group" Mongo.=:
+    [ "_id"   Mongo.=: Mongo.String "$package"
+    , "total" Mongo.=: ["$sum" Mongo.=: Mongo.String "$count"]
+    ] ]
+
+aggrSort :: Mongo.Document
+aggrSort = ["$sort" Mongo.=: ["total" Mongo.=: Mongo.Int64 (-1)]]
+
+aggrLimit :: Int -> Mongo.Document
+aggrLimit lim = ["$limit" Mongo.=: Mongo.Int64 (fromIntegral lim)]
